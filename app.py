8569ea3fbc4f6f56f0ba069931f28f43983e6620
natavelandia <39436355+natavelandia@users.noreply.github.com>
2023-06-07 01:46:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import requests, json
 import numpy as np
 import pickle
-import joblib
+
 
 from flask import Flask,render_template
 from sklearn.linear_model import Ridge
@@ -12,12 +12,24 @@ from sklearn.decomposition import PCA
 from flask import Flask, render_template, request, session
 
 app= Flask (__name__)
+global modelo
+global scalar
+global pca
 
+with open('ridge.pkl', 'rb') as archivo:
+    modelo = pickle.load(archivo)
+with open('scaler.pkl', 'rb') as archivo:
+    scalar = pickle.load(archivo)
+with open('pca.pkl', 'rb') as archivo:
+    pca = pickle.load(archivo)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -30,18 +42,24 @@ def home():
         balance = request.form['balance']
         general = request.form['general']
         #'Aroma', 'Flavor', 'Aftertaste', 'Acidity', 'Body', 'Balance', 'Overall'
-        
-
-
-        
 
 
 
+        feature=np.asarray([aroma,sabor,regusto,acidez,cuerpo,balance,general])
+        # con 8.58,8.50,8.42,8.58,8.25,8.42,8.58 da 89.33
+        feat_html=feature
+        feature=feature.reshape(1, -1)
+        print(feature.shape)
+        scaler = scalar.transform(feature)
+        pc = pca.transform(scaler)
+        prediction = np.round(modelo.predict(pc),2)
 
-        return render_template('index.html', feature=aroma, puntaje=aroma)
+        print(prediction)
+
+
+        return render_template('index.html', feature=feat_html, puntaje=prediction)
 
     return render_template('index.html')
 
 if __name__=="__main__":
-
     app.run(debug=True)
